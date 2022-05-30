488f4ad284f1aebd0ea19f5d74ec92d24d96afac
Bhanujit Halder <88885935+Bhanujit@users.noreply.github.com>
2022-05-30 16:48:37 +0530

    Add files via upload
    did as much as ican

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,35 +1,53 @@
-import React from "react";
+import React,{useState,useEffect}from "react"; 
+import {Button} from '@chakra-ui/react'
+import styled from 'styled-components'
 
+import axios from "axios";
 const AddProduct = () => {
-  // TODO: Remove below const and instead import them from chakra
-  const Button = () => <div />;
-  const Modal = () => <div />;
-  const ModalBody = () => <div />;
-  const Input = () => <div />;
-  const Select = () => <div />;
-  const RadioGroup = () => <div />;
-  const Radio = () => <div />;
-
+  const [forms, setForms] = useState({})
+  function handleForm (e) {
+    const { name, value} = e.target
+    setForms({...forms, [name]:value})
+  }
+  function handleSubmit(e){
+    e.preventDefault()
+    axios.post(`http://localhost:8080/products`, {
+      ...forms
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+  
+  const Input = styled.input`
+    border: 2px solid black;
+  `
   return (
     <>
       <Button my={4} data-cy="add-product-button"></Button>
-      <Modal>
-        <ModalBody pb={6}>
-          <Input data-cy="add-product-title" />
-          <Select data-cy="add-product-category">
-            <option data-cy="add-product-category-shirt"></option>
-            <option data-cy="add-product-category-pant"></option>
-            <option data-cy="add-product-category-jeans"></option>
-          </Select>
-          <RadioGroup data-cy="add-product-gender">
-            <Radio data-cy="add-product-gender-male"></Radio>
-            <Radio data-cy="add-product-gender-female"></Radio>
-            <Radio data-cy="add-product-gender-unisex"></Radio>
-          </RadioGroup>
-          <Input data-cy="add-product-price" />
-          <Button data-cy="add-product-submit-button"></Button>
-        </ModalBody>
-      </Modal>
+     
+        <form  onSubmit={handleSubmit}>
+          Title:{""}
+          <Input type="text" name="title" onChange={handleForm}/>
+          Price:{""}
+          <Input type="text" name ="price" onChange={handleForm} />
+          <select name="gender" id="" onChange={handleForm}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+          <select name="categroy" id="" onChange={handleForm}>
+            <option value="jeans">Jeans</option>
+            <option value="trouser">Trouser</option>
+            <option value="t-shirt">T-shirt</option>
+            <option value="shirt">shirt</option>
+
+          </select>
+          <Input type="submit" />
+        </form>
+     
     </>
   );
 };
